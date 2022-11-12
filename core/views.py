f07cf8d5aa5654364eb92530e450909618d09311
Gigleydson Dias <gigleydson@gmail.com>
2022-11-12 11:16:29 -0300

    Configuração da área administrativa, criação de usuários, inserção de itens e Django Shell

--- core/views.py
+++ core/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 
 
 def index(request):
+    print(dir(request.user))
+    print(f'User: {request.user}')
+
+    if str(request.user) == 'AnonymousUser':
+        user = 'Usuário não logado'
+    else:
+        user = 'Usuário logado'
+
     context = {
         'curso': 'Programação web com Django Framework',
         'outro': 'Django é muito foda!',
-        'git': 'Teste de commit',
+        'logado': user,
     }
     return render(request, 'index.html', context)
 
