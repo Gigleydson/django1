dbcb700a746ef343a342caab5334982fc7b42562
Gigleydson Dias <gigleydson@gmail.com>
2022-11-13 13:41:36 -0300

    Atualização do projeto django 1. Mostrando dados no navegador

--- core/views.py
+++ core/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render
 
+from .models import Produto
+
 
 def index(request):
-    print(dir(request.user))
-    print(f'User: {request.user}')
-
-    if str(request.user) == 'AnonymousUser':
-        user = 'Usuário não logado'
-    else:
-        user = 'Usuário logado'
+    produtos = Produto.objects.all()
 
     context = {
         'curso': 'Programação web com Django Framework',
         'outro': 'Django é muito foda!',
-        'logado': user,
+        'produtos': produtos,
     }
     return render(request, 'index.html', context)
 
 
 def contato(request):
     return render(request, 'contato.html')
+
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+
+    context = {
+        'produto': prod,
+    }
+    return render(request, 'produto.html', context)
